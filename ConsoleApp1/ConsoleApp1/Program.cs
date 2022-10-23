ef83b94e90c6213518b62400ddd325775d78eaa1
Егор Рудов <91718087+RudovEgor@users.noreply.github.com>
2022-10-23 11:44:57 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,80 +1,101 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        static int[] ShowArray(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                 Console.Write(myArray[i]+" "); 
+            }
+            Console.WriteLine();
+           return myArray;
+        }
+        static void firstWayToSolveArray(int[] myArray)
+        {
+            int MaxValue1 = 0;
+            int MaxValue2 = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                if (MaxValue1 < myArray[i])
+                {
+                    MaxValue2 = MaxValue1;
+                    MaxValue1 = myArray[i];
+
+                }
+                if (MaxValue2 < myArray[i] && MaxValue1 != myArray[i])
+                {
+                    MaxValue2 = myArray[i];
+                }
+
+            }
+            Console.WriteLine(MaxValue1);
+            Console.WriteLine(MaxValue2);
+        }
+        static void secondWayTiSolveArray(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                for (int j = i + 1; j < myArray.Length; j++)
+                {
+                    if (myArray[i] == myArray[j])
+                    {
+                        Console.WriteLine("Массивы у которых есть одинаковые элементы: " + myArray[i]);
+                    }
+                }
+            }
+        }
+        static void thirdWayToSolveArray()
+        {
+            Console.WriteLine("Введите кол-во элементов в массиве");
+            int elementCount = int.Parse(Console.ReadLine());
+            int[] Array = new int[elementCount];
+            Random rnd = new Random();
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                int r = rnd.Next(1, 10);
+                Array[i] = r;
+                Console.Write(" " + r);
+
+            }
+        }
         static void Main(string[] args)
         {
-            int[] myArray = {1,2,2,3,40,5,6,6,7,10,121 };
+            int[] myArray = { 1, 2, 2, 3, 40, 5, 6, 6, 7, 10, 121 };
+            ShowArray(myArray);
             Console.WriteLine("Привет выберите что хотите сделать с массивом\n1:Нахождение 2-ух положительных массивов\n2:Есть ли в массиве одинаковые элементы\n3:Заполнение массива случайными числами");
             int UserChoice = 0;
             do
             {
                 UserChoice = int.Parse(Console.ReadLine());
-                
+
                 if (UserChoice == 1)
                 {
-                    int MaxValue1 = 0;
-                    int MaxValue2 = 0;
-                    for (int i = 0; i < myArray.Length; i++)
-                    {
-                        
-                        if (MaxValue1 < myArray[i])
-                        {
-                            MaxValue2 = MaxValue1;
-                            MaxValue1 = myArray[i];
-
-                        }
-                        if (MaxValue2 < myArray[i] && MaxValue1 != myArray[i])
-                        {
-                            MaxValue2 = myArray[i];
-                        }
-                        
-                    }
-                    Console.WriteLine(MaxValue1);
-                    Console.WriteLine(MaxValue2);
+                    firstWayToSolveArray(myArray);
                     break;
                 }
                 if (UserChoice == 2)
                 {
-                    for (int i = 0; i < myArray.Length; i++)
-                    {
-                        
-                        for (int j = i+1; j < myArray.Length; j++)
-                        {
-                            if (myArray[i] == myArray[j])
-                            {
-                                Console.WriteLine("\n"+myArray[i]);
-                                Console.WriteLine("\n" + myArray[j]);
-                            }
-                        }
-                    }
+                    secondWayTiSolveArray(myArray);
                     break;
                 }
                 if (UserChoice == 3)
                 {
-                    Console.WriteLine("Введите кол-во элементов в массиве");
-                    int elementCount = int.Parse(Console.ReadLine());
-                    int[] Array = new int[elementCount];
-                    Random rnd = new Random();
-                    
-                    for (int i = 0; i < Array.Length; i++)
-                    {
-                        int r = rnd.Next(1, 10);
-                        Array[i] = r;
-                        Console.WriteLine("\n"+r);
-                        
-                    }
-                    
+
+                    thirdWayToSolveArray();
                     break;
                 }
                 else
                 {
                     Console.WriteLine("Вы не выбрали способ , повторите еще раз");
                 }
-            } while (UserChoice != 1 || UserChoice != 2  || UserChoice != 3);
-            
+            } while (UserChoice != 1 || UserChoice != 2 || UserChoice != 3);
+
             Console.ReadKey();
         }
     }
